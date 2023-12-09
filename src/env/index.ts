b1a71d1af68a8fa9beef4577b81f4f31e31c4e69
Tuag <igortuag@gmail.com>
2023-12-09 07:17:36 -0300

    add throw error

--- src/env/index.ts
+++ src/env/index.ts
@@ -7,3 +7,9 @@ const envSchema = z.object({
 });
 
 const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.log("Invalid environment variables", _env.error.format());
+  
+  throw new Error("Invalid environment variables");
+}
