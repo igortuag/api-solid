b42d48c6c20151ec46da784b5a97ee48985c85ad
Tuag <igortuag@gmail.com>
2023-12-09 07:17:50 -0300

    export env

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,8 @@ const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
   console.log("Invalid environment variables", _env.error.format());
-  
+
   throw new Error("Invalid environment variables");
 }
+
+export const env = _env.data;
