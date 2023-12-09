c247f44113d8cf1be0a90b3063accad9e8c93301
Tuag <igortuag@gmail.com>
2023-12-09 07:16:40 -0300

    valid schema

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,3 +5,5 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3333)
 });
+
+const _env = envSchema.safeParse(process.env);
