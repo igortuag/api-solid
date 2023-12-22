97c65c2bf1a97eb563f62a37c6eba7e1caad0bfb
Tuag <igortuag@gmail.com>
2023-12-22 08:52:18 -0300

    add new router to post new user

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 export const app = fastify()
 
 const prisma = new PrismaClient()
+
+app.post('/users', (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+})
