97e6228bca1e6a63e20767ac9d3d1082c96a41a4
Tuag <igortuag@gmail.com>
2023-12-24 06:49:32 -0300

    separete register controler

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,8 @@ import fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import { z } from 'zod'
 import { prisma } from './lib/prisma'
+import { register } from './http/controlers/register'
 
 export const app = fastify()
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
-  const { name, email, password } = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
-
-  return reply.status(201).send()
-})
+app.post('/users', register)
