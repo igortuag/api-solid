8c5192047afcabe6d233a825d8ebaa1be71badf2
Tuag <igortuag@gmail.com>
2023-12-24 06:53:52 -0300

    separete route file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
-import { prisma } from './lib/prisma'
-import { register } from './http/controlers/register'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-app.post('/users', register)
+app.register(appRoutes)
