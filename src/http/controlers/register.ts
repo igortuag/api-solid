60dbb895b3b96b3fcea13715c6d2e3cdf6d8d0b3
Tuag <igortuag@gmail.com>
2023-12-27 08:46:55 -0300

    use usecase on route

--- src/http/controlers/register.ts
+++ src/http/controlers/register.ts
@@ -1,18 +1,27 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
-import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
+import { prisma } from "@/lib/prisma";
+import { registerUseCase } from "@/use-cases/register";
+import { hash } from "bcryptjs";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
-    password: z.string().min(6),
-  })
+    password: z.string().min(6)
+  });
 
-  const { name, email, password } = registerBodySchema.parse(request.body)
+  const { name, email, password } = registerBodySchema.parse(request.body);
 
- 
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password
+    });
+  } catch (error) {
+    return reply.status(400).send();
+  }
 
-  return reply.status(201).send()
+  return reply.status(201).send();
 }
