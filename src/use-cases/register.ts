1bbee9db0bfb123b6ba7a74e1388d80c5512f447
Tuag <igortuag@gmail.com>
2023-12-28 09:23:48 -0300

    use prisma user repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,10 +1,11 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
+import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { hash } from "bcryptjs";
 
 interface RegisterUseCaseRequest {
-  name: string
-  email: string
-  password: string 
+  name: string;
+  email: string;
+  password: string;
 }
 
 export async function registerUseCase({
@@ -12,23 +13,23 @@ export async function registerUseCase({
   email,
   password
 }: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+  const password_hash = await hash(password, 6);
 
   const useWithSameEmail = await prisma.user.findUnique({
     where: {
-      email,
-    },
-  })
+      email
+    }
+  });
 
   if (useWithSameEmail) {
-    throw new Error('Email already exists')
+    throw new Error("Email already exists");
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
+  const prismaUsersRepository = new PrismaUsersRepository();
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash
+  });
 }
