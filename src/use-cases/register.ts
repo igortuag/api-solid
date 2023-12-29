2d0a5a52ec925e2bc47e60345f1c475bba5438f4
Tuag <igortuag@gmail.com>
2023-12-29 09:06:31 -0300

    adjust class to use repository as instance

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,39 +1,33 @@
-import { prisma } from "@/lib/prisma";
-import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
-import { hash } from "bcryptjs";
+import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
-  name: string;
-  email: string;
-  password: string;
+  name: string
+  email: string
+  password: string
 }
 
-class RegisterUseCase {
+export class RegisterUseCase {
   constructor(private usersRepository: PrismaUsersRepository) {}
 
-  async execute({
-    name,
-    email,
-    password
-  }: RegisterUseCaseRequest) {
-    const password_hash = await hash(password, 6);
-  
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
     const useWithSameEmail = await prisma.user.findUnique({
       where: {
-        email
-      }
-    });
-  
+        email,
+      },
+    })
+
     if (useWithSameEmail) {
-      throw new Error("Email already exists");
+      throw new Error('Email already exists')
     }
-  
-    const prismaUsersRepository = new PrismaUsersRepository();
-  
-    await prismaUsersRepository.create({
+
+    await this.usersRepository.create({
       name,
       email,
-      password_hash
-    });
+      password_hash,
+    })
   }
 }
