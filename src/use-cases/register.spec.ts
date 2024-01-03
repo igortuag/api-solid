abaa7f61356cfbf0a53eaf4e15aa2b2b86d58e74
Tuag <igortuag@gmail.com>
2024-01-03 09:16:09 -0300

    add example of register use

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,3 +1,18 @@
 import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
-describe('Register Use Case', () => {})
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    const user = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.password).not.toBe('123456')
+  })
+})
