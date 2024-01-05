6d32425a7ef9599b816a2c6f036fbb3b82f18ffb
Tuag <igortuag@gmail.com>
2024-01-05 09:15:47 -0300

    add test to check if the user is create with the same email

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryRepository } from '@/repositories/in-memory/in-memory-user-repository'
+import { UserAlreadyExistError } from './errors/user-already-exist-error'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
@@ -21,5 +22,27 @@ describe('Register Use Case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not allow registration of users with same email', async () => {
+    const userRepository = new InMemoryRepository()
+
+    const registerUseCase = new RegisterUseCase(userRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistError)
   })
 })
