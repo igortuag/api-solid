47af3c454dc9e1ffdb19b29f8be6d3eac0e08bd4
Tuag <igortuag@gmail.com>
2024-01-05 08:58:27 -0300

    use in memory repository

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,24 +1,13 @@
 import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
+import { InMemoryRepository } from '@/repositories/in-memory/in-memory-user-repository'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(email) {
-        return null
-      },
+    const inMemoryRepository = new InMemoryRepository()
 
-      async create(data) {
-        return {
-          id: 'user-1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-    })
+    const registerUseCase = new RegisterUseCase(inMemoryRepository)
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',
