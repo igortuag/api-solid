7b22754741464a5e34df99947784ee3682c52932
Tuag <igortuag@gmail.com>
2024-01-08 09:36:49 -0300

    add interfaces to the use case

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -1,9 +1,21 @@
 import { UsersRepository } from '@/repositories/user-repository'
 
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  token: string
+}
+
 export class AuthenticateUseCase {
   constructor(private readonly userRepository: UsersRepository) {}
 
-  async execute() {
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
     // auth process
   }
 }
