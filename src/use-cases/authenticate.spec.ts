043f67e4255a41cee59aa359a8464029e81decf4
Tuag <igortuag@gmail.com>
2024-01-09 09:05:19 -0300

    test error to non exist user

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from 'vitest'
 import { InMemoryRepository } from '@/repositories/in-memory/in-memory-user-repository'
 import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate Use Case', () => {
   it('should be able to authenticate', async () => {
@@ -20,5 +21,17 @@ describe('Authenticate Use Case', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const userRepository = new InMemoryRepository()
+    const sut = new AuthenticateUseCase(userRepository)
+
+    expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
