f31574da61d52da2171de91b24e6e9d18b3bf559
Tuag <igortuag@gmail.com>
2024-01-09 09:03:26 -0300

    create user before test

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,14 +1,18 @@
 import { expect, describe, it } from 'vitest'
-import { RegisterUseCase } from './register'
-import { compare } from 'bcryptjs'
 import { InMemoryRepository } from '@/repositories/in-memory/in-memory-user-repository'
-import { UserAlreadyExistError } from './errors/user-already-exist-error'
 import { AuthenticateUseCase } from './authenticate'
+import { hash } from 'bcryptjs'
 
 describe('Authenticate Use Case', () => {
   it('should be able to authenticate', async () => {
     const userRepository = new InMemoryRepository()
     const sut = new AuthenticateUseCase(userRepository)
+
+    await userRepository.create({
+      name: 'John Doe',
+      email: '123456',
+      password_hash: hash('123456', 6),
+    })
 
     const { user } = await sut.execute({
       email: 'johndoe@example.com',
