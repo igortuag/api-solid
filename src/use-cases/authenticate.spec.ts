4ec40563fa874d5253aba647a6d14785b95339db
Tuag <igortuag@gmail.com>
2024-01-09 09:05:54 -0300

    test wrong password case

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -34,4 +34,27 @@ describe('Authenticate Use Case', () => {
       }),
     ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
+
+  it('should be not able to authenticate with wrong password', async () => {
+    const userRepository = new InMemoryRepository()
+    const sut = new AuthenticateUseCase(userRepository)
+
+    await userRepository.create({
+      name: 'John Doe',
+      email: '123456',
+      password_hash: hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123123',
+    })
+
+    expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
 })
