a367c54a06b542280a57e7bf6b069210b6125e05
Tuag <igortuag@gmail.com>
2024-01-18 08:47:52 -0300

    add first test to check in

---
+++ src/use-cases/check-in-spec.ts
@@ -0,0 +1,24 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { AuthenticateUseCase } from './authenticate'
+import { hash } from 'bcryptjs'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-in'
+
+let checkInRepository: InMemoryCheckInsRepository
+let sut: CheckInUseCase
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    checkInRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(checkInRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'any_gym_id',
+      userId: 'any_user_id',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
