ad60e0d38935043bc7f5f7cb532563fd18283fd0
Tuag <igortuag@gmail.com>
2024-01-28 07:50:30 -0300

    add test to check in distant check in

--- src/use-cases/check-in-spec.ts
+++ src/use-cases/check-in-spec.ts
@@ -79,4 +79,26 @@ describe('Authenticate Use Case', () => {
 
     await expect(checkIn.id).toEqual(expect.any(String))
   })
+
+  it('should not be able to check in on distant gym', async () => {
+    vi.setSystemTime(new Date('2024-01-01 10:00:00'))
+
+    gymsRepository.items.push({
+      id: 'any_gym_id_2',
+      title: 'any_name',
+      phone: 'any_phone',
+      description: 'any_description',
+      latitude: new Decimal(-27.0108),
+      longitude: new Decimal(-49.544602),
+    })
+
+    await expect(
+      await sut.execute({
+        gymId: 'any_gym_id_2',
+        userId: 'any_user_id',
+        userLatitude: -27.2108,
+        userLongitude: -49.644602,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
 })
