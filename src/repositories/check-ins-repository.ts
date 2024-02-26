b67c6e63000d66e6e83678385dbc75cab4811568
Tuag <igortuag@gmail.com>
2024-02-26 06:44:46 -0300

    add new method to check in repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
+  findById(id: string): Promise<CheckIn | null>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   countByUserId(userId: string): Promise<number>
