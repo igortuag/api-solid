b8c93a613966146b1f9278fce25675e9023ce7d6
Tuag <igortuag@gmail.com>
2024-03-17 07:20:00 -0300

    implement save method

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,37 +1,48 @@
-import { Prisma } from "@prisma/client";
-import { CheckInsRepository } from "../check-ins-repository";
-import { prisma } from "@/lib/prisma";
+import { Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { prisma } from '@/lib/prisma'
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
   async findById(id: string) {
     const checkIn = await prisma.checkIn.findUnique({
-      where: { id }
-    });
+      where: { id },
+    })
 
-    return checkIn;
+    return checkIn
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
-    const checkIn = await prisma.checkIn.create({ data });
+    const checkIn = await prisma.checkIn.create({ data })
 
-    return checkIn;
+    return checkIn
   }
+
   async save(checkIn: {
-    id: string;
-    created_at: Date;
-    validation_at: Date | null;
-    user_id: string;
-    gym_id: string;
+    id: string
+    created_at: Date
+    validation_at: Date | null
+    user_id: string
+    gym_id: string
   }) {
-    throw new Error("Method not implemented.");
+    const updatedCheckIn = await prisma.checkIn.update({
+      where: { id: checkIn.id },
+      data: {
+        validation_at: checkIn.validation_at,
+      },
+    })
+
+    return updatedCheckIn
   }
+
   async findManyByUserId(userId: string, page: number) {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
+
   countByUserId(userId: string): Promise<number> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
+
   findByUserIdOnDate(userId: string, date: Date) {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
 }
