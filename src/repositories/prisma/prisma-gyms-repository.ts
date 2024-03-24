353db9caa2987a4e9fe8a08a15d35d596103210e
Tuag <igortuag@gmail.com>
2024-03-24 07:04:24 -0300

    implement method find gym by id

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -6,7 +6,13 @@ import {
 
 export class PrismaGymsRepository implements GymsRepository {
   async findById(id: string) {
-    throw new Error('Method not implemented.')
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
   }
 
   async findManyNearby(params: FetchNearbyhGymUseCaseRequest) {
