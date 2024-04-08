aa72242eec0b5b215e2966df5b286d535c5b0fea
Tuag <igortuag@gmail.com>
2024-04-08 09:10:53 -0300

    add new router to get user profile authenticated

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controlers/register'
 import { authenticate } from './controlers/authenticate'
+import { profile } from './controlers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
