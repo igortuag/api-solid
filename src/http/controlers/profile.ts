c9821f798aa4b39811d75463cc3d3c8bf1e883a1
Tuag <igortuag@gmail.com>
2024-04-19 08:37:52 -0300

    add use case to get user profile

--- src/http/controlers/profile.ts
+++ src/http/controlers/profile.ts
@@ -4,5 +4,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
+  const getUserProfile = makeGetUserProfileUseCase()
+
   return reply.status(200).send()
 }
