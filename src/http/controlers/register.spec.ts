6dad342763e6e66fa87cc948e3b904d6d04a8fe2
Tuag <igortuag@gmail.com>
2024-05-14 08:41:35 -0300

    add after all test to close the app

--- src/http/controlers/register.spec.ts
+++ src/http/controlers/register.spec.ts
@@ -1,10 +1,14 @@
 import request from 'supertest'
 import { app } from '@/app'
-import { beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Register (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
   })
 
   it('should be able to register', async () => {
