07909e397c949ffdcd563bfb5c5a36ad8e8aad40
Tuag <igortuag@gmail.com>
2024-05-29 08:42:25 -0300

    add routes controllers

--- src/http/controlers/check-ins/routes.ts
+++ src/http/controlers/check-ins/routes.ts
@@ -3,9 +3,14 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
 import { validate } from './validate'
+import { history } from './history'
+import { metric } from './metrics'
 
 export async function checkInRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metric)
 
   app.post('/check-ins/:gymId/check-ins', create)
   app.patch('/check-ins/:checkInId/validate', validate)
